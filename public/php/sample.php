788cf0d34ca60a7aed06ee9073cf19b9816a2e49
hyspxt <patrick.alfieri@studio.unibo.it>
2024-08-25 18:18:59 +0200

    quick css fixes

--- public/php/sample.php
+++ public/php/sample.php
@@ -32,8 +32,7 @@ function getTableData($db, $table)
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Progetto Basi di Dati</title>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../css/style.css">
+
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
@@ -43,6 +42,11 @@ function getTableData($db, $table)
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
     <link href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css" rel="stylesheet" type="text/css" />
+
+    <!-- Inclusions for css and fonts  -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 </head>
 
 <body>
@@ -56,12 +60,12 @@ function getTableData($db, $table)
 
     <div class="content" style="font-family: 'Roboto', sans-serif;">
         <form method="POST" action="">
-            <select name="select_table" id="select_table" class="formQuery" onchange="this.form.submit()">
+            <select name="select_table" id="select_table" class="formSelezioneLabel" onchange="this.form.submit()">
                 <option value=""> Seleziona una tabella </option>
                 <?php
                 $results = getTables($db);
                 while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
-                    /* mantain current choide */
+                    /* mantain current choice */
                     $selected = (isset($_POST['select_table']) && $_POST['select_table'] === $row['name']) ? 'selected' : '';
                     echo '<option value="' . $row['name'] . '" ' . $selected . '>' . $row['name'] . '</option>';
                 }
